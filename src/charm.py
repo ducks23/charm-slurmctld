6188fc2d2e05931c658086db5b012a8c6cf6c8e3
jesse <jesse1517@gmail.com>
2020-06-16 01:30:19 +0000

    coming together

--- src/charm.py
+++ src/charm.py
@@ -13,16 +13,42 @@ from ops.model import ActiveStatus, MaintenanceStatus
 
 from interface_host_port import HostPort
 
+from slurm_snap_instance_manager import SlurmSnapInstanceManager
+
+from adapters.framework import FrameworkAdapter
+
+
 class SlurmctldCharm(CharmBase):
+    slurm_instance_manager_cls = SlurmSnapInstanceManager
 
     def __init__(self, *args):
         super().__init__(*args)
-
+        self.slurm_snap = self.slurm_instance_manager_cls(self, "slurmdbd")
+        self.fw_adapter = FrameworkAdapter(self.framework)
         self.hp = HostPort(self, "host-port")
-        self.framework.observe(self.on.install, self.on_install)
 
-    def on_install(self, event):
+        event_handler_bindings = {
+            self.on.install: self._on_install,
+            self.hp.on.host_port_available: self._on_host_port_available
+        }
+        for event, handler in event_handler_bindings.items():
+            self.fw_adapter.observe(event, handler)
+
+
+    def _on_install(self, event):
+        handle_install(
+            event,
+            self.fw_adapter,
+            self.slurm_snap,
+        )
+    def _on_host_port_available(self, event):
         pass
+
+def handle_install(self, event, fw_adapter):
+    slurm_snap.install()
+    fw_adapter.set_unit_status(ActiveStatus("slurm snap installed"))
+
+
 
 if __name__ == "__main__":
     main(SlurmctldCharm)
--- src/host_port_provides.py
+++ src/host_port_provides.py
@@ -16,14 +16,9 @@ class HostPort(Object):
     def __init__(self, charm, relation_name):
         super().__init__(charm, relation_name)
         self.framework.observe(
-                charm.on[relation_name].relation_joined,
-                self._on_relation_joined
-            )
-        self.framework.observe(
-                charm.on[relation_name].relation_changed,
-                self._on_relation_changed
-            )
-
+            charm.on[relation_name].relation_joined,
+            self._on_relation_joined
+        )
     def _on_relation_joined(self, event):
         event.relation.data[self.model.unit].setdefault('port', '9999')
         event.relation.data[self.model.unit].setdefault('host', 'host')
